a050a73b022679050b179e61267b4b9d91b924b9
LitvinovichPavel <21asdf@mail.ru>
2022-09-23 09:42:02 +0300

    Added changes

---
+++ _TeamCityTest/Schemas/ContactPageV2/ContactPageV2.js
@@ -0,0 +1,60 @@
+define("ContactPageV2", [], function() {
+	return {
+		entitySchemaName: "Contact",
+		attributes: {},
+		modules: /**SCHEMA_MODULES*/{}/**SCHEMA_MODULES*/,
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		businessRules: /**SCHEMA_BUSINESS_RULES*/{}/**SCHEMA_BUSINESS_RULES*/,
+		methods: {},
+		dataModels: /**SCHEMA_DATA_MODELS*/{}/**SCHEMA_DATA_MODELS*/,
+		diff: /**SCHEMA_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "LOOKUP7bbf8874-38f2-4099-a5ac-e195a019877b",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 6,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrForCheck",
+					"enabled": true,
+					"contentType": 3
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 6
+			},
+			{
+				"operation": "merge",
+				"name": "JobTabContainer",
+				"values": {
+					"order": 2
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "HistoryTab",
+				"values": {
+					"order": 5
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "NotesAndFilesTab",
+				"values": {
+					"order": 6
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "ESNTab",
+				"values": {
+					"order": 7
+				}
+			}
+		]/**SCHEMA_DIFF*/
+	};
+});
